9ad9878832c92615c883702f59fdc27990ef3dce
조재철 <rhsnqk@gmail.com>
2022-11-22 15:36:04 +0900

    maven project start

---
+++ src/main/java/com/springtour/travelbooking/TravelBookingApplication.java
@@ -0,0 +1,13 @@
+package com.springtour.travelbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springtour/travelbooking/TravelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springtour.travelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
