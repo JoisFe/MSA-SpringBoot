617fe4963e70a2a42a7846961f11b9170239b11a
조재철 <rhsnqk@gmail.com>
2022-11-24 16:08:00 +0900

    feat(HotelRoom): 호텔 객실 검색 Controller 메서드 구현 [#3]

--- src/main/java/com/springtour/travelbooking/controller/HotelRoomController.java
+++ src/main/java/com/springtour/travelbooking/controller/HotelRoomController.java
@@ -1,7 +1,15 @@
 package com.springtour.travelbooking.controller;
 
+import com.springtour.travelbooking.domain.HotelRoomType;
 import com.springtour.travelbooking.dto.HotelRoomResponse;
+import com.springtour.travelbooking.utils.IdGenerator;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,5 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class HotelRoomController {
 
     @GetMapping(path = "/hotels/{hotelId}/rooms/{roomNumber}")
-    public HotelRoomResponse getHotelRoomByPeriod()
+    public HotelRoomResponse getHotelRoomByPeriod(@PathVariable Long hotelId, @PathVariable String roomNumber,
+        @RequestParam(value = "fromDate", required = false) @DateTimeFormat(pattern = "yyyyMMdd")
+            LocalDate fromDate,
+        @RequestParam(value = "toDate", required = false) @DateTimeFormat(pattern = "yyyyMMdd") LocalDate toDate) {
+        Long hotelRoomId = IdGenerator.create();
+        BigDecimal originalPrice = new BigDecimal("130.00");
+
+        HotelRoomResponse response = HotelRoomResponse.of(hotelRoomId, roomNumber, HotelRoomType.DOUBLE, originalPrice);
+
+        if (Objects.nonNull(fromDate) && Objects.nonNull(toDate)) {
+            fromDate.datesUntil(toDate.plusDays(1))
+                    .forEach(response::reservedAt);
+        }
+
+        return response;
+    }
 }
