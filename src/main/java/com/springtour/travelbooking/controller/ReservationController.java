f66b8bddc0eaec0c772424fa967d7448340cc159
조재철 <rhsnqk@gmail.com>
2022-11-26 23:20:02 +0900

    feat: HttpMessageConverter를 이용한 미디어 콘텐츠 내려받기 [#16]
     HttpMessageConverter를 이용해 미디어 콘텐츠 내려받기 구현
    - @ResponseEntity 혹은 그와 같은 기능하는 @RestController 설정
    - 메서드 시그니처의 응답 객체는 byte[] 혹은 ResponseEntity<byte[]> 형태로

--- src/main/java/com/springtour/travelbooking/controller/ReservationController.java
+++ src/main/java/com/springtour/travelbooking/controller/ReservationController.java
@@ -1,8 +1,14 @@
 package com.springtour.travelbooking.controller;
 
+import com.springtour.travelbooking.exception.FileDownloadException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +31,21 @@ public class ReservationController {
         });
 
         return Collections.emptyList();
+    }
+
+    @GetMapping("/hotels/{hotelId}/rooms/{roomNumber}/reservations/{reservationId}")
+    public ResponseEntity<byte[]> getInvoice(@PathVariable Long hotelId, @PathVariable String roomNumber,
+        @PathVariable Long reservationId) {
+        String filePath = "pdf/hotel_invoice.pdf";
+
+        try (InputStream inputStream = new ClassPathResource(filePath).getInputStream()) {
+            byte[] bytes = StreamUtils.copyToByteArray(inputStream);
+
+            return new ResponseEntity<>(bytes, HttpStatus.OK);
+        } catch (Throwable th) {
+            th.printStackTrace();
+
+            throw new FileDownloadException("file download error");
+        }
     }
 }
