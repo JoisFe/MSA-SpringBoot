5576bc25e827b7bdb3d2888379cd7eaa290819e1
조재철 <rhsnqk@gmail.com>
2022-11-28 20:43:09 +0900

    feat: Swagger 추가 [#18]
     Swagger 추가
    - Swagger 의존성 추가
    - Swagger 설정 자바 파일을 통해 설정

--- src/main/java/com/springtour/travelbooking/controller/HotelRoomController.java
+++ src/main/java/com/springtour/travelbooking/controller/HotelRoomController.java
@@ -7,6 +7,7 @@ import com.springtour.travelbooking.dto.HotelRoomRequest;
 import com.springtour.travelbooking.dto.HotelRoomResponse;
 import com.springtour.travelbooking.dto.HotelRoomUpdateRequest;
 import com.springtour.travelbooking.utils.IdGenerator;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
