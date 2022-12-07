8931b4db715d57d7c7c4e339296558fd9fd9dcc9
조재철 <rhsnqk@gmail.com>
2022-12-07 23:53:45 +0900

    style: 필요 없는 import 삭제

--- src/main/java/com/springtour/travelbooking/config/WebServerConfig.java
+++ src/main/java/com/springtour/travelbooking/config/WebServerConfig.java
@@ -1,10 +1,6 @@
 package com.springtour.travelbooking.config;
 
-import java.util.List;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
